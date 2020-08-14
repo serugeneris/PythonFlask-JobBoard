55dabdce4b4272496b06e7b74d048dc36884615b
serugeneris <tute_2193@hotmail.com>
2020-08-14 20:27:12 -0300

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -4,9 +4,10 @@ import sqlite3
 app = Flask(__name__)
 
 def open_connection():
-	connection = getattr(g._connection,default=None)
+	connection = getattr(g, '_connection',None)
 	if connection == None:
-		connection, g._connection = sqlite3.connect(PATH)
+		connection = sqlite3.connect(PATH)
+		g._connection = sqlite3.connect(PATH)
 	connection.row_factory = sqlite3.Row
 	return connection
 
@@ -18,12 +19,13 @@ def execute_sql(sql,values=(),commit=False,single=False):
 		results = connection.commit()
 	else:
 		results = cursor.fetchone() if single else cursor.fetchall()
+	cursor.close()
 	return results
 
-@app.teardown_appcontext()
+@app.teardown_appcontext
 def close_connection(exception):
 	connection = getattr(g,'_connection',None)
-	if connection not None:
+	if connection != None:
 		connection.close()
 
 
